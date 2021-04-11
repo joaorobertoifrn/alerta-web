fa99346cd23bbda1b78b837dd29951208e686b7f
root <root@ip-172-31-77-50.ec2.internal>
2021-04-11 01:05:36 +0000

    atualização

--- dist/polyfills.js
+++ dist/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\jpereir3\Bitbucket\TCC\alerta-frontend-web\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! /home/ubuntu/alerta-frontend-web/src/polyfills.ts */"hN/g");
 
 
 /***/ }),
